40d5a29866f75018171de8d526c1a60cce6dd8b8
Jonathan Chan <johnkchan252@gmail.com>
2018-08-29 01:25:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,14 +14,7 @@ class Song
   end
   
   def self.new_by_filename(file_name)
-    file_name = file_name.split(" - ")
-    song_name = file_name[1]
-    
-    artist = Artist.find_or_create_by_name(file_name[0])
-    song = Song.new(song_name)
-    song.artist = artist
-    artist.add_song(song)
-    song
+ 
   end
   
 end
